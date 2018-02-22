405c61ffb42b89d9c5f10d1300ea39e3673e8ae0
Rajitha <rajitha.rmn@gmail.com>
2018-02-21 19:57:07 -0800

    Added signup page, js file and css edits

---
+++ js/form-handler.js
@@ -0,0 +1,10 @@
+function capture() {
+  event.preventDefault() //stops form submit
+  console.log("Form submitted")
+
+  var firstName = document.custInfo.userFirstname.value;
+  var lastName = document.custInfo.userLastname.value;
+  var eMail = document.custInfo.userLastname.value;
+  console.log(firstName, lastName, eMail);
+  console.log("Thank you, " + firstName + lastName + " for signing up! Look out for exciting offers in your email!");
+}

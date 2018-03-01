1df23e98fb41572468dc94546f0c3dbc4bce11c0
Rajitha <rajitha.rmn@gmail.com>
2018-02-28 18:43:56 -0800

    edit form-handler

--- js/form-handler.js
+++ js/form-handler.js
@@ -4,7 +4,7 @@ function capture() {
 
   var firstName = document.custInfo.userFirstname.value;
   var lastName = document.custInfo.userLastname.value;
-  var eMail = document.custInfo.userLastname.value;
+  var eMail = document.custInfo.userEmail.value;
   console.log(firstName, lastName, eMail);
-  console.log("Thank you " + firstName + " " + lastName + " for signing up! Look out for exciting offers in your inbox!");
+  console.log("Thank you " + firstName + " " + lastName + "for signing up. Look for exciting offers in your inbox!");
 }
